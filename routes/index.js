326fe44e57a52395d5b260db67a9a190b773e429
mildsummer <mild.summer.y@gmail.com>
2015-10-14 14:06:10 +0900

    ユーザー登録

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("index");
   console.log(req.session.passport);
-  res.render('index', {
-    title: 'login demo',
-    session: req.session.passport //passportでログイン後は、このオブジェクトに情報が格納されます。
+  var passport = req.session.passport;
+  User.findOne({_id: req.session.passport.id}).exec(function(err, user) {
+    if(!err) {
+      if(!user) {//初めてログインした場合はユーザー情報を保存
+        var user = new User({_id: passport.id, name: passport.screen_name});
+        user.save(function(err) {
+          if(!err) {
+            res.render('index', {
+              title: 'ユーザー登録を完了しました',
+              session: req.session.passport
+            });
+          } else {
+            res.render('error', {
+              error: err,
+              message: 'ユーザー登録エラー'
+            });
+          }
+        });
+      } else {
+        res.render('index', {
+          title: 'ユーザー登録済みです',
+          session: req.session.passport
+        });
+      }
+    } else {
+      res.render('error', {
+        error: err,
+        message: 'ユーザー参照エラー'
+      });
+    }
   });
 });
 
