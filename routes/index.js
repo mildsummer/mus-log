cabe0940bdcada6b777c9e1fecd9873191047777
mildsummer <mild.summer.y@gmail.com>
2015-10-14 12:53:16 +0900

    デバグ

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("index");
+  console.log(req.session.passport);
   res.render('index', {
     title: 'login demo',
     session: req.session.passport //passportでログイン後は、このオブジェクトに情報が格納されます。
